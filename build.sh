68ea9848cd8e2e1223162697efbfdcfb345cee0a
CennacEh <cennadude@gmail.com>
2025-09-01 15:17:32 +0100

    Upload files

---
+++ build.sh
@@ -0,0 +1,3 @@
+clear
+g++ src/index.cpp -o app -lraylib -lopengl32 -lgdi32 -lwinmm -ldsound -mwindows
+./app
---
+++ src/index.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+
+#include "raylib.h"
+
+int main(int argc, char* argv[]) {
+    InitWindow(800, 450, "Window");
+    Vector2 rectangle[2] = { { 0.00f, (float)GetScreenHeight() - 150 }, { (float)GetScreenWidth(), 10 } };
+    Vector2 cube = { (float)GetScreenWidth()/2, (float)GetScreenHeight()/2 };
+    
+    Camera2D cam = { 0 };
+    cam.target = cube;
+    cam.offset = (Vector2){ GetScreenWidth()/2.0f, GetScreenHeight()/2.0f };
+    cam.rotation = 0.0f;
+    cam.zoom = 1.0f;
+
+    float velocity = 0;
+    while (!WindowShouldClose()) {
+        ClearBackground(RAYWHITE);
+        cam.offset = (Vector2){ cube.x, cube.y };
+        rectangle[0] = { 0.00f, (float)GetScreenHeight() - 150 };
+        rectangle[1] = { (float)GetScreenWidth(), 10 };
+        if (IsKeyDown(KEY_D)) cube.x += 0.15;
+        if (IsKeyDown(KEY_A)) cube.x -= 0.15;
+        if (rectangle[0].y - 30 <= cube.y) {
+            velocity = 0;
+            if (IsKeyDown(KEY_SPACE) || IsKeyDown(KEY_W)) velocity = 0.25;
+        } else {
+            velocity -= 0.001;
+        }
+        if (cube.x <= -30) {
+            cube.x = GetScreenWidth() + 30;
+        } else if (cube.x >= GetScreenWidth() + 30) {
+            cube.x = -30;
+        }
+        cube.y -= velocity;
+        BeginDrawing();
+        {
+            DrawRectangleV(rectangle[0], rectangle[1], RED);
+            DrawRectangleV(cube, { 30, 30 }, ORANGE);
+        }
+        EndDrawing();
+    }
+    CloseWindow();
+    return 0;
+}
